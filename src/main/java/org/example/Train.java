395883042bab9692d0a7b33991ed29040140c33e
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-05 13:05:40 +0700

    Скомпилировал и подключил модуль 2, создал Train

---
+++ src/main/java/org/example/Train.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// TODO: 10.09.2022 Описать класс Train, потомок класса Transport из модуля 2.
+// TODO: 10.09.2022 Описать свойства String id, int carriageCount, boolean isExpress.
+
+public class Train {
+}
