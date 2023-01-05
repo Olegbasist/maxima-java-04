fa85d6f4cc69f3556e9a93b9c469edbd0256ca0a
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-05 17:33:32 +0700

    ...

--- src/main/java/org/example/Train.java
+++ src/main/java/org/example/Train.java
@@ -2,7 +2,9 @@ package org.example;
 
 //  10.09.2022 Описать класс Train, потомок класса Transport из модуля 2.
 //  10.09.2022 Описать свойства String id, int carriageCount, boolean isExpress.
+// TODO: 13.10.2022 Использовать аннотации Lombok для добавления всех геттеров, сеттеров, метода toString() и конструктора со всеми аргументами
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +16,7 @@ public class Train extends Transport {
     private String id;
     private int carriageCount;
     private boolean isExpress;
+    // Следующие свойства пришлось добавить, чтобы их подцепил Lombok
     private String name;
     private int capacity;
     private int speed;
