eaec8ba7d0003404d779a8b1cb5fe15261c81768
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-06 14:20:34 +0700

    Добавил аннотацию @ToString

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,12 +15,13 @@ public class App
 
         City city = new City("Washington D.C.",6847);
         Train parovoz = new Train("1", "Паровоз", false, 950000, 8,60,1.5f);
-        System.out.println(parovoz.getName() +" чих-пых = "+ parovoz.getPrice(city));
+        //System.out.println(parovoz.getName() +" чих-пых = "+ parovoz.getPrice(city));//После анотирования lombok перестало работать
         System.out.println("------------------------------------------------------");
         parovoz.setId("4");
         parovoz.setName("Чих-Чихыч");
         parovoz.setCostOfKm(1);
-        System.out.println(parovoz.getName() + parovoz.getId() + parovoz.isExpress() + parovoz.getCarriageCount());
+        
+        System.out.println(parovoz.toString());
         System.out.println(parovoz.getName() +" чих-пых = "+ parovoz.getPrice(city));
     }
 }
--- src/main/java/org/example/Train.java
+++ src/main/java/org/example/Train.java
@@ -7,16 +7,18 @@ package org.example;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
-
+@ToString
 public class Train extends Transport {
 
     private String id;
     private int carriageCount;
     private boolean isExpress;
-    // Следующие свойства пришлось добавить, чтобы их подцепил Lombok
+
+    // Остальные свойства пришлось добавить, чтобы их подцепил Lombok
     private String name;
     private int capacity;
     private int speed;
