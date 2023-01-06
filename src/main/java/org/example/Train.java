fb02a817fd8c852ee11a7fe66ff367f16d708d50
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-06 15:04:39 +0700

    Заработал ломбочный конструктор

--- src/main/java/org/example/Train.java
+++ src/main/java/org/example/Train.java
@@ -2,41 +2,30 @@ package org.example;
 
 //  10.09.2022 Описать класс Train, потомок класса Transport из модуля 2.
 //  10.09.2022 Описать свойства String id, int carriageCount, boolean isExpress.
-// TODO: 13.10.2022 Использовать аннотации Lombok для добавления всех геттеров, сеттеров, метода toString() и конструктора со всеми аргументами
+//  13.10.2022 Использовать аннотации Lombok для добавления всех геттеров, сеттеров, метода toString() и конструктора со всеми аргументами
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor // Пришлось добавить пустой конструктор в Transport
+
 public class Train extends Transport {
 
+    // Чтобы их подхватил Lombok нужны все свойства.
+    // Порядок объявления влияет на порядок в ломбочном конструкторе.
     private String id;
-    private int carriageCount;
-    private boolean isExpress;
-
-    // Остальные свойства пришлось добавить, чтобы их подцепил Lombok
     private String name;
+    private boolean isExpress;
     private int capacity;
+    private int carriageCount;
     private int speed;
     private float costOfKm;
 
-    public Train(String id, String name, boolean isExpress, int capacity, int carriageCount, int speed, float costOfKm) {
-        super(name, capacity, speed, costOfKm);//Lombok не подхватывает "предковые" свойства
-        this.id = id;
-        this.carriageCount = carriageCount;
-        this.isExpress = isExpress;
-        //   Пришлось добавить для Lombok
-        this.name = name;
-        this.capacity = capacity;
-        this.speed = speed;
-        this.costOfKm = costOfKm;
-    }
 
 
+    // Доломбочный конструктор
     /*public Train(String id, String name, boolean isExpress, int capacity, int carriageCount, int speed, float costOfKm) {
 
         super(name, capacity, speed, costOfKm);
